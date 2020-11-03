bead3e5fc45d35dad2b8b0d388f89afd24477e08
matejerceg <me51860@fer.hr>
2020-11-03 08:39:56 +0100

    added springboot project

---
+++ src/main/java/hr/fer/ilj/iljgrupa2/IljGrupa2Application.java
@@ -0,0 +1,13 @@
+package hr.fer.ilj.iljgrupa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IljGrupa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IljGrupa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/ilj/iljgrupa2/IljGrupa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.ilj.iljgrupa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IljGrupa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
